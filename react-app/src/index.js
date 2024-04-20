a0b4b406bee670afe388011aae18f4f7d1151f9e
ncmarker <ncmarker@usc.edu>
2024-04-20 16:24:07 -0700

    testing initial api call

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+
+import './styles/globals.css';  // Legacy or third-party CSS styles
+import './styles/main.scss';   // SCSS styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
 
