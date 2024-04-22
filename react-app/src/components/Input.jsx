65c692314dbf472c875c8fd55d9628df9b6f2d29
sifawangia1 <123137142+sifawangia1@users.noreply.github.com>
2024-04-22 03:26:49 -0700

    added third page

--- react-app/src/components/Input.jsx
+++ react-app/src/components/Input.jsx
@@ -1,6 +1,6 @@
 const Input = (props) => {
   return (
-    <input className="bg-white-gradient-20 text-white/60 px-5 py-2 rounded-[8px]" placeholder={props.placeholder} value={props.value} onChange={(e) => props.setValue(e.target.value)} />
+    <input className="bg-grey-gradient-20 text-white/60 px-5 py-2 rounded-[8px]" placeholder={props.placeholder} value={props.value} onChange = {(e) => props.setValue(e.target.value)} />
   )
 }
 
